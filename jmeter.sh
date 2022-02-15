700d4d0d8ce5783b0aec08940142733a3d960afb
sshivank <s.shivank@samsung.com>
2022-02-15 16:18:13 +0530

    script added

---
+++ jmeter.sh
@@ -0,0 +1,11 @@
+sudo docker run \
+        --interactive \
+        --tty \
+        --rm \
+        --volume 'pwd':/jmeter \
+        egaillardon/jmeter \
+        --nongui \
+        --testfile test.jmx \
+        --logfile result.jtl \
+        --reportatendofloadtests \
+        --reportoutputfolder report
